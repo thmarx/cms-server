dab5934a71f61c90692a2985380764688520af42
Thorsten Marx <thmarx@gmx.net>
2023-11-26 15:42:48 +0100

    call callback on error

--- cms-server/src/main/java/com/github/thmarx/cms/server/jetty/handler/JettyDefaultHandler.java
+++ cms-server/src/main/java/com/github/thmarx/cms/server/jetty/handler/JettyDefaultHandler.java
@@ -73,6 +73,7 @@ public class JettyDefaultHandler extends Handler.Abstract {
 			log.error("", e);
 			response.setStatus(500);
 			response.getHeaders().add("Content-Type", "text/html; charset=utf-8");
+			callback.succeeded();
 		}
 		return true;
 	}
