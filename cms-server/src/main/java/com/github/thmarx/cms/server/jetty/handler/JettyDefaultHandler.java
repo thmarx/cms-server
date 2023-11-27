c1891647ab49cd06b1ce5a530c8775171f304997
Thorsten Marx <t.marx@eggheads.de>
2023-11-27 13:13:06 +0100

    call callback

--- cms-server/src/main/java/com/github/thmarx/cms/server/jetty/handler/JettyDefaultHandler.java
+++ cms-server/src/main/java/com/github/thmarx/cms/server/jetty/handler/JettyDefaultHandler.java
@@ -73,6 +73,8 @@ public class JettyDefaultHandler extends Handler.Abstract {
 			log.error("", e);
 			response.setStatus(500);
 			response.getHeaders().add("Content-Type", "text/html; charset=utf-8");
+			
+			callback.succeeded();
 		}
 		return true;
 	}
