108539861248017048e51552c96a5eff96a1d31c
Thorsten Marx <t.marx@eggheads.de>
2023-12-23 18:44:07 +0100

    #133 add function to generate key for captcha

---
+++ modules/forms-module/src/main/java/com/github/thmarx/cms/modules/forms/utils/StringUtil.java
@@ -0,0 +1,46 @@
+package com.github.thmarx.cms.modules.forms.utils;
+
+/*-
+ * #%L
+ * forms-module
+ * %%
+ * Copyright (C) 2023 Marx-Software
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import java.util.Random;
+
+/**
+ *
+ * @author t.marx
+ */
+public class StringUtil {
+
+	static Random random = new Random();
+
+	public static String random_string() {
+		int leftLimit = 48; // numeral '0'
+		int rightLimit = 122; // letter 'z'
+		int targetStringLength = 10;
+
+		return random.ints(leftLimit, rightLimit + 1)
+				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+				.limit(targetStringLength)
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+				.toString();
+	}
+}
