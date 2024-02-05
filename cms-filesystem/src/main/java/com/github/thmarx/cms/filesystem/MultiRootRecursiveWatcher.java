d24cbcba8dace6a87cf5d54ebc81ba7f3aba3f8b
Thorsten Marx <t.marx@eggheads.de>
2024-02-05 09:21:37 +0100

    #168 catch exception

--- cms-filesystem/src/main/java/com/github/thmarx/cms/filesystem/MultiRootRecursiveWatcher.java
+++ cms-filesystem/src/main/java/com/github/thmarx/cms/filesystem/MultiRootRecursiveWatcher.java
@@ -50,8 +50,6 @@ import java.util.TimerTask;
 import java.util.concurrent.Flow;
 import java.util.concurrent.SubmissionPublisher;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -153,6 +151,8 @@ public class MultiRootRecursiveWatcher {
 						resetWaitSettlementTimer();
 					} catch (InterruptedException | ClosedWatchServiceException e) {
 						running.set(false);
+					} catch (Exception e) {
+						log.error("an error occured", e);
 					}
 				}
 			}
