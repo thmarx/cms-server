b2cab675e888c4792dee6f900f94bc6a51edc0b5
Thorsten Marx ㋡ <thmarx@gmx.net>
2026-08-27 15:09:12 +0200

    fix error in logging.mjs (#760)

--- test-server/hosts/demo/extensions/site.hooks.js
+++ test-server/hosts/demo/extensions/site.hooks.js
@@ -1,7 +1,11 @@
 import { $hooks } from 'system/hooks.mjs';
+import { getLogger } from 'system/logging.mjs'
 
+
+const LOGGER = getLogger("extensions");
 
 $hooks.registerAction("theme/template/header", (context) => {
+    LOGGER.info("site: logging from a extension");
 	return "<!-- header 1 -->";
 })
 $hooks.registerAction("theme/template/header", (context) => {
